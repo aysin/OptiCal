8b24d49ad777cb7feff801207fa2a05aa2ade8db
Aysin <aysin.oruz@gmail.com>
2017-05-14 15:47:32 +0200

    Add jinja and html template

--- optical/app.py
+++ optical/app.py
@@ -1,14 +1,23 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, html, text
 from elasticsearch import Elasticsearch
+from jinja2 import Environment, PackageLoader
 
 
 client = Elasticsearch(hosts=['elasticsearch'], http_auth=('elastic', 'changeme',))
-app = Sanic()
+env = Environment(loader=PackageLoader('app', 'templates'))
 
-@app.route("/")
+app = Sanic(__name__)
+
+@app.route('/')
 async def test(request):
-    return json({"hello": "world"})
+    data = {'name': 'Aysin'}
+
+    template = env.get_template('index.html')
+    html_content = template.render(name=data["name"])
+    # content=template.render(title='Sanic',people='David')
+    return html(html_content)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
